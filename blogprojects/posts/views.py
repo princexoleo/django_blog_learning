a68dbe3f1b0ab8d1ed56fe9bd49d9487cf1f39af
maz_leo <xtleon.xl@gmail.com>
2019-07-12 22:11:30 +0600

    template/posts/index html created

--- blogprojects/posts/views.py
+++ blogprojects/posts/views.py
@@ -4,4 +4,5 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     pass
-    return HttpResponse('Hello Posts')
+    #return HttpResponse('Hello Posts')
+    return render(request, 'posts/index.html')
