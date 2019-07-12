b0e6b64fef780e3250595d25afaf427a123453f6
maz_leo <xtleon.xl@gmail.com>
2019-07-13 00:14:13 +0600

    assign homepage to post

--- blogprojects/blogprojects/urls.py
+++ blogprojects/blogprojects/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+     path('', include('posts.urls')),
     path('posts/', include('posts.urls')), #goto posts url
 ]
