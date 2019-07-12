a605418b7161d51fdca593ce78a7ede323e387fb
maz_leo <xtleon.xl@gmail.com>
2019-07-13 00:37:32 +0600

    createdetails  method

--- blogprojects/posts/views.py
+++ blogprojects/posts/views.py
@@ -1,8 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Posts
 
 # Create your views here.
 def index(request):
-    pass
+    posts = Posts.objects.all()[:10] #Post objects fetch and frist 10 post fetch
+    context = {
+        'title':'Latest Post',
+        'posts': posts,
+    }
     #return HttpResponse('Hello Posts')
-    return render(request, 'posts/index.html',{'title':'Latest Post'})
+    return render(request, 'posts/index.html',context)
+
+
+def details(request, id):
+    post = Posts.objects.get(id=id)
+
+    context ={
+        'post': post,
+    }
+
+    return render(request,'posts/details.html', context)
